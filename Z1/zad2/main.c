df697540872a4f6b8665fa7e055771dbc9530f51
korneliarohullko <korneliarohulko@gmail.com>
2018-03-11 23:22:12 +0100

    Add time measurement

--- Z1/zad2/main.c
+++ Z1/zad2/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <sys/times.h>
 
 void test_dynamic_library(int n, int sizeofBlock, int array[], int numofOperations);
 void test_static_library(int n, int sizeofBlock, int array[], int numofOperations);
@@ -14,6 +15,15 @@ void realloc_blocks_sequentially(struct CharArray* charArray);
 void realloc_blocks_alternately(struct CharArray* charArray);
 int* getRange(struct CharArray* charArray);
 
+
+void print_time(clock_t startTime,clock_t stopTime, struct tms a, struct tms b){
+    long double real,user,sys;
+    real = (long double)(stopTime - startTime) / (long double)CLOCKS_PER_SEC;
+    user = (long double)(b.tms_utime - a.tms_utime) / (long double)CLOCKS_PER_SEC;
+    sys = (long double)(b.tms_stime - a.tms_stime) / (long double)CLOCKS_PER_SEC;
+    printf("real:%LF user:%LF sys:%LF\n", real,user,sys);
+}
+
 /* 1 - static memory allocation, 0 - dynamic memory allocation,
 n - numbers of blocks, sizeofBlock - size of single block, array - list of operations,
 numofOperations - numbers of operations in array */
@@ -24,13 +34,29 @@ void test_library(int n, int sizeofBlock, int memAllocation, int array[], int nu
     if(memAllocation) {
         test_dynamic_library(n, sizeofBlock, array, numofOperations);
     } else {
-        test_static_library(n, sizeofBlock, array, numofOperations);
+        //test_static_library(n, sizeofBlock, array, numofOperations);
     }
 }
 
 void test_dynamic_library(int n, int sizeofBlock, int array[], int numofOperations) {
     if(array[0] == 1) {
-        CharArray* charArray = initialize_dynamic_array(n, sizeofBlock);
+
+        //*************************************
+        //create - start time
+        printf("\ntesting create array - dynamic allocation\n");
+        clock_t startTime,stopTime;
+        struct tms a,b;
+        startTime = clock();
+        times(&a);
+
+        struct CharArray* charArray = initialize_dynamic_array(n, sizeofBlock);
+
+        //create - stop
+        stopTime = clock();
+        times(&b);
+        print_time(startTime,stopTime,a,b);
+        //create -end
+        //**************************************
         execute_operations_dynamic(array, numofOperations, charArray);
     } else {
         printf("You have to initalize an array first.");
@@ -40,15 +66,50 @@ void test_dynamic_library(int n, int sizeofBlock, int array[], int numofOperatio
 void execute_operations_dynamic(int array[], int numofOperations, struct CharArray* charArray) {
     int i;
     for(i = 0; i < numofOperations; i++) {
+        clock_t startTime,stopTime;
+        struct tms a,b;
         switch (array[i]) {
             case 2:
+                //*************************************
+                printf("\ntesting node searching - dynamic allocation\n");
+                startTime = clock();
+                times(&a);
+
                 search_node(charArray, generate_random_num(100));
+
+                stopTime = clock();
+                times(&b);
+                print_time(startTime,stopTime,a,b);
+                //**************************************
                 break;
             case 3:
+                //*************************************
+                printf("\ntesting realloc_blocks_sequentially - dynamic allocation\n");
+                startTime = clock();
+                times(&a);
+
                 realloc_blocks_sequentially(charArray);
+
+                stopTime = clock();
+                times(&b);
+                print_time(startTime,stopTime,a,b);
+                //**************************************
+
                 break;
             case 4:
+
+                //*************************************
+                printf("\ntesting realloc_blocks_alternately - dynamic allocation\n");
+                startTime = clock();
+                times(&a);
+
                 realloc_blocks_alternately(charArray);
+
+
+                stopTime = clock();
+                times(&b);
+                print_time(startTime,stopTime,a,b);
+                //**************************************
                 break;
         }
     }
@@ -88,7 +149,7 @@ void realloc_blocks_sequentially(struct CharArray* charArray) {
     }
 }
 
-CharArray* initialize_dynamic_array(int n, int sizeofBlock) {
+struct CharArray* initialize_dynamic_array(int n, int sizeofBlock) {
     struct CharArray* charArray = create_char_arr(n);
     int i;
     for(i = 0; i < n; i++) {
@@ -119,5 +180,9 @@ int generate_random_num(int maxval) {
 
 int main(void) {
     srand(time(NULL));
-
+    int* array = (int*) calloc(3, sizeof (int));
+    array[0] = 1;
+    array[1] = 2;
+    array[2] = 4;
+    test_library(10000, 5000, 1, array, 3);
 }
